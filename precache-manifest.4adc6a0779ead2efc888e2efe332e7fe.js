995e6a7619fc2c75316ead5213b44eaf8c11168f
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-08-23 17:23:59 +0200

    Updates

---
+++ precache-manifest.4adc6a0779ead2efc888e2efe332e7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a96c59eae20d146d2e8f7864a73ce62",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "33f76a5e3edc6d0b598d",
+    "url": "/react-weather-app/static/css/main.732d652d.chunk.css"
+  },
+  {
+    "revision": "e6076259839f6b689fd8",
+    "url": "/react-weather-app/static/js/2.a63107ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.a63107ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f76a5e3edc6d0b598d",
+    "url": "/react-weather-app/static/js/main.2ad2d877.chunk.js"
+  },
+  {
+    "revision": "47b15cfe188b344dd729",
+    "url": "/react-weather-app/static/js/runtime-main.b9fe54b6.js"
+  }
+]);
